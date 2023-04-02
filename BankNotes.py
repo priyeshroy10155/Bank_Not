17f2a4031272026e17c4132c2367448324bbfbe4
Priyesh Kumar Roy <100074623+priyeshroy10155@users.noreply.github.com>
2023-04-02 19:15:14 +0530

    commit
    commit

--- BankNotes.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-# class with describes Bank note measurements
-class BankNote(BaseModel):
-    variance: float
-    skewness: float
-    curtosis: float
-    entropy:  float
-    text:bool
